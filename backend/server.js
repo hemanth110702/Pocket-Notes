b4f3ef6d1c89af7ab51ab264dbd384c5dab43d8d
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-25 19:51:50 +0530

    updated production links

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/user/", userRoute);
+app.use("/api/user", userRoute);
 app.use("/api/notes", noteRoute);
 
 mongoose
