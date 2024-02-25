4a14770f5b112c3487aaf946e459d36a57b8e0da
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2024-02-25 18:56:54 +0530

    updated all origins

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Authorization", "Content-Type"],
   })
